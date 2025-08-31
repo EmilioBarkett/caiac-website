828bf676043f56a661e71e12f18be193de7be38e
龙 <141962390+long-olivia@users.noreply.github.com>
2025-08-31 14:52:30 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default async function Home() {
             <p className="text-lg leading-relaxed text-gray-600">
               We also run a semester-long introductory reading group on AI safety, including both a <a href="/get_involved/technical-fellowship" className="text-[#4A8A99] hover:underline">technical machine learning track</a>, and a <a href="/get_involved/policy-fellowship" className="text-[#4A8A99] hover:underline">policy track</a>.
             </p>
-            <p><strong>Applications for both our fellowships are now closed! Please look out for us next semester!</strong></p>
+            <p><strong>Applications for both our fellowships are now open! Apply <a href="https://forms.gle/ESS1suCsBoGvva9v7">here</a> for policy, and <a href="https://forms.gle/2F5dw4Dm9NTAabqe9">here</a> for technical.</strong></p>
           </div>
 
           {/* Logos Section */}
